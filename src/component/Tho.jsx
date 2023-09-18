f8923c877750d68e6d4a2ebd95f893e6f96fb5fe
kongtuk2539 <thoty2539@gmail.com>
2023-09-18 11:05:14 +0700

    commit mix
    commit 1

---
+++ src/component/Tho.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tho = () => {
+    return (
+        <div>
+            Tho
+            <p>มาแล้วจ้าาา</p>
+            <p>มาแล้วจ้าาา</p>
+            <p>มาแล้วจ้าาา</p>
+        </div>
+    )
+}
+
+export default Tho
