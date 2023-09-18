f361437af6e5f791fe58776eb0a63d64efa70e18
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-09-18 10:28:51 +0700

    edit footer
    add new line

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <h1>Footer Mook</h1>
+      <p>testtest</p>
+      <p>new line</p>
+    </div>
+  )
+}
+
+export default Footer
