f093fde2f4b9d17f9041d5982dc2f2911de8f19d
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-09-18 10:40:37 +0700

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,12 +4,7 @@ function Footer() {
   return (
     <div>
       <h1>Footer Mook</h1>
-<<<<<<< Updated upstream
-      <p>testtest</p>
-      <p>new line</p>
-=======
       <p>new lineeeeee</p>
->>>>>>> Stashed changes
     </div>
   )
 }
